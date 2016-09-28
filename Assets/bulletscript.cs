2d91adb1e9ed3e3de36e7e614ec9bc7569d644da
Falsan <sam.britnell117@gmail.com>
2016-09-28 15:44:11 +0100

    Derpyshooter Commit

---
+++ Assets/bulletscript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletscript : MonoBehaviour {
+
+    float speed = -4.0f;
+    float deathTimer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        deathTimer++;
+
+        Vector3 newPosition = new Vector3(0, getSpeed(), 0);
+
+        transform.Translate(newPosition * Time.deltaTime);
+
+        if(getTimer() > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void setSpeed(float speedToSet)
+    {
+        speed = speedToSet;
+    }
+
+    float getSpeed()
+    {
+        return speed;
+    }
+
+    float getTimer()
+    {
+        return deathTimer;
+    }
+
+    void setTimer(float timerToSet)
+    {
+        deathTimer = timerToSet;
+    }
+}
