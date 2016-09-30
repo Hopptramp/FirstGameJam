bc5d08ac08acf8994f2daece077f01177973d7f1
Adster94 <adam.moorey@hotmail.co.uk>
2016-09-30 14:19:38 +0100

    StateMachine start.

--- Assets/GUI/Scripts/GUIScript.cs
+++ Assets/GUI/Scripts/GUIScript.cs
@@ -1,63 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GUIScript : MonoBehaviour {
+public class GUIScript : MonoBehaviour
+{
 
-	int score = 0;
-	float time = 0.0f;
+    int score = 0;
+    float time = 0.0f;
 
-	bool climbing = true;
-	bool paused = false;
+    bool climbing = true;
+    bool paused = false;
 
-	// Use this for initialization
-	void Start () 
-	{
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		//if the current state is play, do this
-		//{
-			if (Input.GetKey (KeyCode.Space)) {
-				climbing = false;
-				print ("climbing now false");
-			}
+    StateMachine sm;
+    GameObject gameManager;
 
-			if (Input.GetKey (KeyCode.X)) {
-				climbing = true;
-				print ("climbing now true");
-			}
+    //error just cause
 
-			ScoreCounter ();
+    // Use this for initialization
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameController");
+        sm = gameManager.gameObject.GetComponent<StateMachine>();
+    }
 
-			TimeTracker ();
-		//}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (sm.currentState == StateMachine.State.Play)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                climbing = false;
+                print("climbing now false");
+            }
 
-	void OnGUI()
-	{
-		//if the state is play, do this
-		//{
-			GUI.Box (new Rect (10, 10, 150, 25), new GUIContent ("Current Score is " + score));
-			GUI.Box (new Rect (Screen.width - 160, 10, 150, 25), new GUIContent ("Time: " + time.ToString ("F2")));
-		//}
-	}
+            if (Input.GetKey(KeyCode.X))
+            {
+                climbing = true;
+                print("climbing now true");
+            }
 
-	void ScoreCounter()
-	{
-		if (climbing == true) {
-			score++;
-		} else {
-			score--;
-		}
-	}
+            ScoreCounter();
 
-	void TimeTracker()
-	{
-		if (!paused) {
-			time += Time.deltaTime;
-		}
-	}
+            TimeTracker();
+        }
+    }
+
+    void OnGUI()
+    {
+        if (sm.currentState == StateMachine.State.Play)
+        {
+            GUI.Box(new Rect(10, 10, 150, 25), new GUIContent("Current Score is " + score));
+            GUI.Box(new Rect(Screen.width - 160, 10, 150, 25), new GUIContent("Time: " + time.ToString("F2")));
+        }
+    }
+
+    void ScoreCounter()
+    {
+        if (climbing == true)
+        {
+            score++;
+        }
+        else
+        {
+            score--;
+        }
+    }
+
+    void TimeTracker()
+    {
+        if (!paused)
+        {
+            time += Time.deltaTime;
+        }
+    }
 }
---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateMachine : MonoBehaviour {
+
+	public enum State {
+		Menu,
+		Play,
+		GameOver
+	}
+
+	public State currentState;
+
+	void Start()
+	{
+		currentState = State.Play;
+	}
+
+	void Update()
+	{
+		if (currentState == State.Menu) {
+			//go through the menu sequence
+		} else if (currentState == State.Play) {
+			//play the main game
+		} else if (currentState == State.GameOver) {
+			//play game over menu
+		}
+	}
+}
