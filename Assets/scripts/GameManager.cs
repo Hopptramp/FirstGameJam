2a943e9696500b78d88dbb7ad074605dd40a2bad
harryjamesdavies <daviesharry1@gmail.com>
2016-09-30 10:17:27 +0100

    Procedural Generated Level.

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public LevelManager levelScript;
+
+    //private int level = 3;
+
+	// Use this for initialization
+	void Awake () {
+        levelScript = GetComponent<LevelManager>();
+        InitGame();
+	}
+
+    void InitGame()
+    {
+        levelScript.SetupScene();
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
