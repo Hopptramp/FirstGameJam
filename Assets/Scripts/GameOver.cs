d250506d0fc549528050032254dd51eb3a4f8826
Adster94 <adam.moorey@hotmail.co.uk>
2016-09-30 11:12:49 +0100

    Added victory and death colliders to the LevelManager - as well as a script for registering these colliders.

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	public GameObject hell;
+	public GameObject heaven;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("Trigger Entered");
+
+		if (other.tag == "DeathCollider") {
+
+			if (other.tag == "Bullet") 
+			{
+				Destroy (other.gameObject);
+			} 
+
+			if (other.tag == "Player") 
+			{
+				Debug.Log ("Player entered destroy collider");
+				//kill player
+			}
+
+		}
+
+		if (other.tag == "VictoryCollider") 
+		{
+
+			if (other.tag == "Player") 
+			{
+				Debug.Log ("Player entered victory collider");
+				//victory to player
+			}
+
+		}
+	}
+}
