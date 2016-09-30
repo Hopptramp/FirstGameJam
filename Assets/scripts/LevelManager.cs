469811d8533aa945e36a491f1726e89006e91aa8
Adster94 <adam.moorey@hotmail.co.uk>
2016-09-30 13:22:21 +0100

    Collider stuff added after fix

--- Assets/scripts/LevelManager.cs
+++ Assets/scripts/LevelManager.cs
@@ -145,11 +145,12 @@ public class LevelManager : MonoBehaviour
         LevelSetup();
         InitialiseList();
         Instantiate(player, new Vector3((columns / 2) - 1.0f, 1.0f, 0.0f), Quaternion.identity);
-        LayoutObjectAtRandom(barrier, barrierCount.minimum, barrierCount.maximum, 5, 2);
-        LayoutObjectAtRandom(lightRay, lightCount.minimum, lightCount.maximum, 1, 10);
 
 		GameObject tempDeathCollider = (GameObject)Instantiate(deathCollider, new Vector3 (0.0f, -5.5f, 0.0f), Quaternion.identity);
 		tempDeathCollider.gameObject.transform.parent = Camera.main.gameObject.transform;
-		Instantiate(victoryCollider, new Vector3 ((columns / 2) - 1.0f, rows, 0.0f), Quaternion.identity); 
+		Instantiate(victoryCollider, new Vector3 ((columns / 2) - 1.0f, rows, 0.0f), Quaternion.identity);
+
+        LayoutObjectAtRandom(barrier, barrierCount.minimum, barrierCount.maximum, 5, 2);
+        LayoutObjectAtRandom(lightRay, lightCount.minimum, lightCount.maximum, 1, 10); 
     }
 }
