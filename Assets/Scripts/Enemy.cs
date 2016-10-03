e7b3de25321bb645405527f99febe762081c6a1e
Falsan <sam.britnell117@gmail.com>
2016-10-03 14:40:42 +0100

    Enemy AI has been fixed and the collisions removed.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject bullet;
+    //public GameObject trackShooter;
+    public GameObject player;
+
+    [SerializeField]
+    public int timeToShoot;
+    [SerializeField]
+    public int shotTimer = 0;
+    [SerializeField]
+    public float speed;
+    public Rigidbody2D _rb;
+    [SerializeField]
+    public bool flip;
+
+    public enum direction //enum which controls direction of derpyshooter
+    {
+        left = 0,
+        right = 1
+    }
+
+    public direction actualDirection = direction.left;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        flip = true;
+        player = GameObject.FindGameObjectWithTag("Player");
+        timeToShoot = 200;
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void fire()
+    {
+        if (timeToShoot != 40) //slowly increases shot rate
+        {
+            timeToShoot = timeToShoot - 10;
+        }
+
+        Vector3 firePort = transform.position;
+        firePort.y = firePort.y - 4;
+        Instantiate(bullet, firePort, Quaternion.identity);
+
+    }
+
+    public void setSpeed(float setToSpeed)
+    {
+        speed = setToSpeed;
+    }
+
+    public float getSpeed()
+    {
+        return speed;
+    }
+
+    public void setTimer(int timerToSet)
+    {
+        shotTimer = timerToSet;
+    }
+
+    public int getTimer()
+    {
+        return shotTimer;
+    }
+}
